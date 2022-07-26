949752cb2840d83671c162ca9ede6b89badae75d
GaurangGhadiya <gaurangghadiya99@gmail.com>
2022-07-27 00:31:54 +0530

    init

---
+++ src/_metronic/_partials/dashboards/Demo1Dashboard.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export function Demo1Dashboard() {
+  return (
+    <>
+      <div className="row h-75">
+        <div className="col-lg-12">
+          <div className="card h-100">
+            <div className="card-body">
+              <div className="row h-75">
+                <div className="col-md-12 text-center d-flex justify-content-center align-items-center ">
+                  <h1>Hello Headspace</h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
